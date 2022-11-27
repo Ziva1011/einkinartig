73401e4d66596bf0990493fcdd10c2525ee89e84
Ziva1011 <catarina.vi.pires@gmail.com>
2022-11-27 22:07:40 +0100

    routes

--- personal-webiste/src/App.js
+++ personal-webiste/src/App.js
@@ -2,18 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import NavMenu from "./components/Navbar"
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Routes} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import DesignChallenge from "./pages/100ui";
+import AboutMe from "./pages/AboutMe";
+import NoPage from "./pages/NoPage";
+import './stylesheets/application.sass'
 
 
 function App() {
   return (
     <div className="App">
-      <>
+      <BrowserRouter>
         <NavMenu/>
         <Routes>
-          
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/100ui" element={<DesignChallenge />} />
+          <Route path="*" element={<NoPage />} />
+        {/* </Route> */}
         </Routes>
-      </>
+      </BrowserRouter>
     </div>
   );
 }
